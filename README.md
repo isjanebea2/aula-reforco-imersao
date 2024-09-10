fe34927da8ff21ebc3cfdd66fdda35a638fea12c
bea ramerindo <beatriz.ramerindo@nuvemshop.com.br>
2024-09-10 19:10:20 -0300

    feat: added basic config for config service

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 
+## Configuração inicial do projeto (init setup)
+- configuração do env
+- configuração do banco de dados
+- remoção de arquivos que não serão utilizados
 
-### references 
+
+#### Instalação da  config Service lib
+
+Permite carregar os valores no .env
+
+[Documentação](https://docs.nestjs.com/techniques/configuration)
+
+```bash
+npm i --save @nestjs/config
+```
+
+### Referências 
 https://reflectoring.io/spring-hexagonal/
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get<ConfigService>(ConfigService)
+
+  const APP_PORT = configService.get<number>('PORT', 3000)
+
+  await app.listen(APP_PORT);
+
+  console.log('App rodando na porta: '+ APP_PORT)
 }
 bootstrap();
